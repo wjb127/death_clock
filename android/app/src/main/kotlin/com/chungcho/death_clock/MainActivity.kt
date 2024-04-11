b54891d5ccfc43be6b855c732a69391da7b91d45
wjb127 <wjb127@naver.com>
2024-04-11 20:24:51 +0900

    버전2

--- android/app/src/main/kotlin/com/chungcho/death_clock/MainActivity.kt
+++ android/app/src/main/kotlin/com/chungcho/death_clock/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.death_clock
+package com.chungcho.death_clock
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class DeathClockApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Death Clock',
       theme: ThemeData(
         primarySwatch: Colors.blue,
