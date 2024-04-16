391124c8d0dd62dc47215bb47d18628c4b0c7229
wjb127 <wjb127@naver.com>
2024-04-16 19:59:22 +0900

    백그라운드 동작 및 인간의 평균수명 80세 명시

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,29 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'dart:async';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+void initializeNotifications() async {
+  var initializationSettingsAndroid =
+      AndroidInitializationSettings('@mipmap/ic_launcher');
+  var initializationSettings =
+      InitializationSettings(android: initializationSettingsAndroid);
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+}
+
+void showPersistentNotification(String title, String body) async {
+  var androidDetails = AndroidNotificationDetails('channel_id', 'channel_name',
+      'This channel is used for important notifications.', // 채널 설명
+      importance: Importance.max,
+      priority: Priority.high,
+      ongoing: true);
+  var platformDetails = NotificationDetails(android: androidDetails);
+  await flutterLocalNotificationsPlugin.show(0, title, body, platformDetails);
+}
 
 // 앱 실행
 void main() {
@@ -38,7 +61,18 @@ class _DeathClockPageState extends State<DeathClockPage> {
   @override
   void initState() {
     super.initState();
+    _loadBirthday();
     _startTimer();
+  }
+
+  void _loadBirthday() async {
+    DateTime? loadedBirthday = await loadBirthday();
+    if (loadedBirthday != null) {
+      setState(() {
+        _birthday = loadedBirthday;
+        _updateTimeLeft();
+      });
+    }
   }
 
   @override
@@ -88,6 +122,11 @@ class _DeathClockPageState extends State<DeathClockPage> {
                     const EdgeInsets.only(top: 20.0), // 남은 수명 텍스트에 상단 패딩 추가
                 child: Text('당신의 남은 수명: ${_formatDuration(_timeLeft!)}'),
               ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+              child: Text('인간의 평균수명 : 80세',
+                  style: TextStyle(fontStyle: FontStyle.italic)),
+            ),
           ],
         ),
       ),
@@ -155,4 +194,15 @@ class _DeathClockPageState extends State<DeathClockPage> {
     final seconds = twoDigits(duration.inSeconds % 60);
     return "$days 일, $hours 시간, $minutes 분, $seconds 초";
   }
+}
+
+void saveBirthday(DateTime birthday) async {
+  final prefs = await SharedPreferences.getInstance();
+  await prefs.setString('birthday', birthday.toIso8601String());
+}
+
+Future<DateTime?> loadBirthday() async {
+  final prefs = await SharedPreferences.getInstance();
+  String? birthdayString = prefs.getString('birthday');
+  return birthdayString != null ? DateTime.parse(birthdayString) : null;
 }
